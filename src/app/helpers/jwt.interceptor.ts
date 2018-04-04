3410f9c22ca395c42ed1283ea39ff62fd482aac0
arnaud <arnaud.bariatti@gmail.com>
2018-04-04 23:03:44 +0200

    modified interceptor

--- src/app/helpers/jwt.interceptor.ts
+++ src/app/helpers/jwt.interceptor.ts
@@ -1,9 +1,15 @@
 ﻿import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/do';
+import { AlertService } from '../services/alert.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
+
+    constructor(private alertService: AlertService, private router:Router) { }
+
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         // add authorization header with jwt token if available
         const currentUser = JSON.parse(localStorage.getItem('currentUser'));
@@ -15,6 +21,17 @@ export class JwtInterceptor implements HttpInterceptor {
             });
         }
 
-        return next.handle(request);
+        return next.handle(request).do((event: HttpEvent<any>) => {
+            // success
+        }, (err: any) => {
+            // error
+            if (err instanceof HttpErrorResponse) {
+                this.alertService.error(err.message);
+                if (err.status === 401) {
+                    // redirect to the login route
+                    this.router.navigate(['login']);
+                }
+            }
+        });;
     }
 }
