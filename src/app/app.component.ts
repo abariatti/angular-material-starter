49671720c2e7617f38014a9951a1452c640b226f
abariatti <abariatti@openwt.com>
2018-11-05 17:21:23 +0100

    improve flex layout on main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, ElementRef, OnInit } from '@angular/core';
 import { OverlayContainer } from '@angular/cdk/overlay';
 import { TranslateService } from '@ngx-translate/core';
 import { ThemePickerService } from './layout/theme-picker/theme-picker.service';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -11,14 +12,24 @@ import { ThemePickerService } from './layout/theme-picker/theme-picker.service';
   styles: []
 })
 export class AppComponent implements OnInit {
-  constructor( private translate: TranslateService,
+  constructor(
+    private translate: TranslateService,
     private element: ElementRef,
     private overlayContainer: OverlayContainer,
-    private themePickerService: ThemePickerService
+    private themePickerService: ThemePickerService,
+    private swUpdate: SwUpdate
   ) {
     this.translate.setDefaultLang('en');
     this.themePickerService.register(this.element, this.overlayContainer);
   }
 
-  ngOnInit() {  }
+  public ngOnInit(): void {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        if (confirm('A new version is available. Load?')) {
+          window.location.reload();
+        }
+      });
+    }
+  }
 }
